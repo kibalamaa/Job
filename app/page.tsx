a331fc56029658b8e08e3776a3cfe7ccdf4c6b02
kibalamaa <jkibalamakatumba@yahoo.com>
2025-11-25 22:49:35 +0300

    reorganize

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,35 @@
-import Button from "./components/Button"
+import Button from "./components/Button";
+import globe from "./../public/globe.svg"
 const Joblist = () => {
   return (
-    <div className="flex flex-col bg-amber-300 w-[600px] h-[200px] rounded-md">
-      <div className="flex flex-row bg-blue-200 h-[30%]">
-        <div className="w-[100px] h-[100%] bg-fuchsia-300">
-          profile image
-        </div>
-        <div className="flex flex-col h-full w-full">
+    <div className="flex flex-row bg-blue-600 w-[600px]">
+      <div className="flex flex-col items-center w-[100px]">
+        <p>progile</p>
+        <img src={globe} />
+      </div>
+      <div className="flex flex-col bg-amber-300 w-[500px] h-[200px]">
+        <div className="flex flex-col h-full w-full bg-amber-600">
           <h2>Social Media Assistant</h2>
+          <p>Sed ut perspiciatis unde omnisequi nesciunt.</p>
+        </div>
+
+        <div className="">
           <p>
-            Sed ut perspiciatis unde omnisequi nesciunt.
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit.
           </p>
         </div>
-        
+        <div className="flex flex-row justify-start  h-[20%]">
+          <Button />
+          <Button />
+          <Button />
+        </div>
       </div>
-      <div className="h-[50%]">
-        <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
-            eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
-            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.
-        </p>
-      </div>
-      <div className="flex flex-row justify-start  h-[20%]">
-        <Button />
-        <Button />
-        <Button />
-      </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Joblist
+export default Joblist;
